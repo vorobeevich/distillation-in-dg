b3f045f0a5f0081a31865c49744b66ee52b2a1ca
vorobeevich <valerygegelevsky@yandex.ru>
2023-02-05 23:39:40 +0300

    Add get device util function

---
+++ src/utils/get_device.py
@@ -0,0 +1,22 @@
+import torch
+
+def get_device(gpu_usage: bool = True) -> torch.device:
+    """Returns the GPU device on which the code will run (if gpu_usage), otherwise the code will be run on the CPU.
+
+    Args:
+        gpu_usage (bool, optional): should the programm use GPU or not. Defatults to True.
+
+    Returns:
+        torch.device: the device on which the code will run.
+    """
+
+    if gpu_usage and torch.cuda.device_count() == 0:
+        print(
+            "Warning: There's no GPU available on this machine,"
+            "training will be performed on CPU."
+        )
+        device = "cpu"
+    else:
+        device = torch.device("cuda:0")
+
+    return device
