e561b5c624e0c6e4ded55372af6354f262fdfd5f
vorobeevich <valerygegelevsky@yandex.ru>
2023-02-06 01:44:46 +0300

    Add init code for train script

---
+++ src/scripts/train.py
@@ -0,0 +1,32 @@
+import argparse
+import collections
+import warnings
+
+import numpy as np
+import torch
+
+from src.utils.fix_seed import fix_seed
+from src.utils.parse_config import parse_config
+
+# fix random seeds for reproducibility
+fix_seed()
+
+parser = argparse.ArgumentParser(description="Train model from config")
+
+parser.add_argument(
+    "--config",
+    default=None,
+    type=str,
+    help="Path to config file",
+    required=True
+)
+parser.add_argument(
+    "--device",
+    default=None,
+    type=str,
+    help="Device index for CUDA_VISIBLE_DEVICES variable",
+    required=True
+)
+
+config = parse_config(parser.parse_args())
+print(config)
