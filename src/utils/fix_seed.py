bd7f27d960f6d0d705cecccceef804421651186c
vorobeevich <valerygegelevsky@yandex.ru>
2023-02-05 23:39:55 +0300

    Add fix seed util function

---
+++ src/utils/fix_seed.py
@@ -0,0 +1,20 @@
+import random
+import os 
+import torch
+import numpy as np
+
+def fix_seed(seed: int = 42) -> None:
+    """Makes the random behavior of some functions deterministic for reproducibility of experiments. 
+    Warning: this function does not provide complete deterministic training of the neural network. 
+     If you are using some functions (like sklearn library) you must manually pass the seed to functions.
+
+    Args:
+        seed (int, optional): seed for fixation. Defatults to 42.
+    """
+
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
