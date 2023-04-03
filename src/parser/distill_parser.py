e76a978938297b6ad79b86fc946b696d7fba4cce
vorobeevich <valerygegelevsky@yandex.ru>
2023-04-04 00:17:08 +0300

    Fix distill parser code style

--- src/parser/distill_parser.py
+++ src/parser/distill_parser.py
@@ -1,18 +1,7 @@
-import os
 import yaml
 import argparse
-from datetime import datetime
-from copy import deepcopy
 import typing as tp
 
-import torchvision.models
-import torchvision.transforms
-import torch.nn as nn
-import torch
-
-from src.logging.wandb import WandbLogger
-from src.utils.get_device import get_device
-from src.utils.init_functions import init_object
 from src.parser.base_parser import BaseParser
 class DistillParser(BaseParser):
     """Static class (a set of methods for which inheritance is possible) for parsing command line arguments 
@@ -21,7 +10,7 @@ class DistillParser(BaseParser):
 
     @staticmethod
     def parse_config(args: argparse.Namespace) -> dict[str, tp.Any]:
-        trainer_params = BaseParser.parse_config(args)
+        trainer_params = super.parse_config(args)
         # read config_yaml from path
         with open(args.config, "r") as stream:
             config = yaml.safe_load(stream)
