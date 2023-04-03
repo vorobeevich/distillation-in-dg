1ead51cd8d29a1427ad92314e1ddbd4dd131beef
vorobeevich <valerygegelevsky@yandex.ru>
2023-04-04 00:16:06 +0300

    Change metric and table logging a bit

--- src/logging/wandb.py
+++ src/logging/wandb.py
@@ -1,4 +1,5 @@
 import wandb
+import pandas as pd
 
 class WandbLogger:
     def __init__(self, config):
@@ -11,12 +12,11 @@ class WandbLogger:
             name=config["run_id"]
         )
 
-    def log_metric(self, test_domain: str, figure_type: str, figure_name: str, value: float, num_step: int):
-        name = f"{test_domain}.{figure_type}.{figure_name}"
+    def log_metric(self, name: str, value: float, num_step: int):
         self.run.log({name:  value, f"{name}.epoch" : num_step})
 
-    def log_table(self, df):
-        self.run.log({"Results": wandb.Table(dataframe=df)})
+    def log_table(self, name: str, df: pd.DataFrame):
+        self.run.log({name: wandb.Table(dataframe=df)})
 
     def log_image(self, image_array, name: str):
         self.run.log({name: wandb.Image(image_array)})
