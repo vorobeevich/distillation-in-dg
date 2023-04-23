34a87b9edebc8a57ebd4ca392acbdbfe555e307e
vorobeevich <valerygegelevsky@yandex.ru>
2023-04-23 04:10:35 +0300

    Add docstrings to PACS

--- src/datasets/PACS_dataset.py
+++ src/datasets/PACS_dataset.py
@@ -1,4 +1,5 @@
 import torch
+import torchvision
 from PIL import Image
 
 
@@ -7,8 +8,8 @@ def get_paths_and_labels(
     """Return list of images paths for a given type of the dataset.
 
     Args:
-        dataset_types (list[str]): one of ["train"], ["test"], ["train", "test"]
-        domain (str): one of "art_painting", "cartoon", "photo", "sketch"
+        dataset_types (list[str]): list of values from {'train', 'test'}
+        domain (str): one of 'art_painting', 'cartoon', 'photo', 'sketch'
 
     Returns:
         tuple[list[str], torch.Tensor]: paths to images and tensor with class labels
@@ -18,7 +19,7 @@ def get_paths_and_labels(
     labels = []
     for ds_type in dataset_types:
         filepath = f"data/pacs/labels/{domain}_{ds_type}.txt"
-        f = open(filepath, "r")
+        f = open(filepath, 'r')
         lines = f.readlines()
         f.close()
         lines = [l.split() for l in lines]
@@ -30,8 +31,27 @@ def get_paths_and_labels(
 
 
 class PACS_dataset(torch.utils.data.Dataset):
-    def __init__(self, dataset_type, domain_list,
-                 transforms, augmentations=None):
+    """Class with standard methods for torch dataset for working with PACS dataset.
+    Inherited from standard class torch.utils.data.Dataset.
+    """
+
+    def init(
+            self,
+            dataset_type: list[str],
+            domain_list: list[str],
+            transforms: torchvision.transforms.Compose,
+            augmentations: torchvision.transforms.Compose = None) -> None:
+        """Dataset initialization. Creates images list (where file paths are stored) 
+        and classes (labels) torch.Tensor for them.
+
+        Args:
+            dataset_types (list[str]): list of values from {'train', 'test'}
+            domain (list[str]): list of values from {'art_painting', 'cartoon', 'photo', 'sketch'}
+            transforms (torchvision.transforms.Compose): transforms that are applied to each
+                image (regardless of whether it is in train or test selection)
+            augmentations (torchvision.transforms.Compose,
+            optional): augmentations that apply only to the train selection. Defaults to None.
+        """
         self.images = []
         self.labels = torch.Tensor([])
         self.domain_list = domain_list
@@ -43,10 +63,28 @@ class PACS_dataset(torch.utils.data.Dataset):
         self.transforms = transforms
         self.augmentations = augmentations
 
-    def __len__(self):
+    def len(self) -> int:
+        """Returns the number of images in the dataset.
+
+        Returns:
+            int: dataset len
+        """
         return len(self.images)
 
-    def __getitem__(self, idx):
+    def getitem(self, idx: int) -> dict[str, torch.Tensor]:
+        """Returns a picture from the dataset by its number.
+        First, the image is read along the path, augmentations are applied to it (if necessary), then transforms.
+        Also, the class label is returned.
+
+        Args:
+            idx (int): index of image
+        Returns:
+            dict[str, torch.Tensor]: dict:
+                {
+                    "image": image torch.Tensor,
+                    "label": label torch.Tensor
+                }
+        """
         img_name = self.images[idx]
         label = self.labels[idx]
 
@@ -54,15 +92,15 @@ class PACS_dataset(torch.utils.data.Dataset):
 
         if self.augmentations:
             sample = {
-                "image":
+                'image':
                 self.augmentations(image)
             }
         else:
             sample = {
-                "image": image,
+                'image': image,
             }
 
-        sample["image"] = self.transforms(sample["image"])
-        sample["label"] = label
+        sample['image'] = self.transforms(sample['image'])
+        sample['label'] = label
 
         return sample
