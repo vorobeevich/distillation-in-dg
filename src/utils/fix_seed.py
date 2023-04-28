dfaf6b2608c96bcec9daf8ea1f9c39326a5bbc9a
vorobeevich <valerygegelevsky@yandex.ru>
2023-04-29 02:33:00 +0300

    Remove fixing seed for torch etc

--- src/utils/fix_seed.py
+++ src/utils/fix_seed.py
@@ -14,8 +14,4 @@ def fix_seed(seed: int = 42) -> None:
     """
 
     random.seed(seed)
-    os.environ["PYTHONHASHSEED"] = str(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+
