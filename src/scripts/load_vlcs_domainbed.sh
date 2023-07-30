0cb7fab344b36ed616d458dfff5d78ad6fec9a77
vorobeevich <valerygegelevsky@yandex.ru>
2023-07-30 23:21:00 +0300

    Add script for vlcs loading

---
+++ src/scripts/load_vlcs_domainbed.sh
@@ -0,0 +1,24 @@
+# create vlcs images data
+
+# make directory for all data
+mkdir -p data/
+
+# make directory for vlcs data
+mkdir -p data/vlcs/
+
+# load from google drive
+echo -e "Start loading images... |"
+curl -o data/vlcs/vlcs_download_file -L "https://drive.google.com/uc?export=download&confirm=yes&id=1skwblH1_okBwxWxmRsp9_qi15hyPpxg8"
+
+# unpacking download file
+echo "Unpacking images... |"
+tar -xvf data/vlcs/vlcs_download_file -C  data/vlcs/
+
+# remove cache file
+rm data/vlcs/vlcs_download_file
+
+# rename VLCS folder to images folder
+mv data/vlcs/VLCS/ data/vlcs/images
+
+# make directory for labels data
+mkdir -p data/vlcs/labels/
